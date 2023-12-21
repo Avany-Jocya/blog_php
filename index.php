7bc02c193e5b12e8a054ec8484cc49d76a713d10
Avany-Jocya <146840337+Avany-Jocya@users.noreply.github.com>
2023-12-21 11:16:51 +0100

    update

--- index.php
+++ index.php
@@ -8,34 +8,19 @@
     <title>Blog_Php</title>
 </head>
 <body>
-   
-<nav class="navbar bg-body-tertiary fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Mon blog</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/a-propos.php">a-propos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="">Link</a>
-          </li>
-          
-        </ul>
-       
-      </div>
-    </div>
-  </div>
-</nav>
+<header>
+<?php include('./inc/templates/navbar.php') ?>
+</header>
 
 <h1>Accueil</h1>
+
+<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+
+
 </body>
 </html>
