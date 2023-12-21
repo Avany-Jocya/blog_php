b58288a8033a243d08bdaa09a1515fca9834fd35
Avany-Jocya <146840337+Avany-Jocya@users.noreply.github.com>
2023-12-21 15:20:52 +0100

    up

--- inscription.php
+++ inscription.php
@@ -1,3 +1,6 @@
 <?php require ('./inc/init.php');?>
 <?php include_once ('./inc/templates/.php');?>
-<?php include_once ('./inc/templates/init.php');?>
+<?php include_once ('./inc/templates/init.php');?><?php require('./inc/init.php');?>
+<?php include_once("./inc/templates/haut.php");?>
+<h1>Inscription</h1>
+<?php include_once("./inc/templates/bas.php");?>
