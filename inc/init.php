73d8a9f7647502c1d017c3bd37215cb622182820
Avany-Jocya <146840337+Avany-Jocya@users.noreply.github.com>
2023-12-21 15:58:24 +0100

    up

--- inc/init.php
+++ inc/init.php
@@ -3,8 +3,10 @@
 // $mysqli = new mysqli("localhost", "db_user", "223105", "blog");
 // if ($mysqli->connect_error) die("un problème est survenu lors de la tentative de connxion a la BDD:.$mysqli->connect_error");
 
-$pdo = new PDO('mysql:host=localhost;dbname=blog', "db_user", "223105", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$pdo = new PDO('mysql:host=localhost;dbname=blog', "root", "Nayah441#", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-$result = $pdo-> exec ("")
+// $result = $pdo->exec("INSERT INTO blog.`user`
+// (pseudo, email, password, id_user)
+// VALUES('test2', 'test1', '123', 0);")
 
 ?>
--- inscription.php
+++ inscription.php
@@ -1,6 +1,4 @@
 <?php require ('./inc/init.php');?>
-<?php include_once ('./inc/templates/.php');?>
-<?php include_once ('./inc/templates/init.php');?><?php require('./inc/init.php');?>
 <?php include_once("./inc/templates/haut.php");?>
 <h1>Inscription</h1>
 <?php include_once("./inc/templates/bas.php");?>
