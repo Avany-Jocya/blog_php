498c623f5a25665825bb1ae91f086ace86a1eb18
Avany-Jocya <146840337+Avany-Jocya@users.noreply.github.com>
2023-12-22 09:42:56 +0100

    up

--- inscription.php
+++ inscription.php
@@ -1,7 +1,7 @@
 <?php require ('./inc/init.php');?>
 
 <?php 
-executeRequete("INSERT INTO utilisateur (id_user, slug,email, passwrd,is_admin) VALUES ('test1', 'test1', '123', 0);")
+executeRequete("INSERT INTO utilisateur (slug,email, passwrd,is_admin) VALUES ('test1', 'test1', '123', 0);")
 
 // executeRequete("DELETE FROM blog.'user' where id_user=1");
 
