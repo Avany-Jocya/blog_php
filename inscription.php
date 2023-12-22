bf13f771227967beed046dd6c089f6d9ebe7c275
Avany-Jocya <146840337+Avany-Jocya@users.noreply.github.com>
2023-12-22 10:34:18 +0100

    up

--- inscription.php
+++ inscription.php
@@ -1,66 +1,42 @@
-<?php require ('./inc/init.php');?>
+<?php require('./inc/init.php');?>
 
 <?php 
-executeRequete("INSERT INTO utilisateur (slug,email, passwrd,is_admin) VALUES ('test1', 'test1', '123', 0);")
+// $result = $pdo->exec("INSERT INTO blog.user
+// (pseudo, email, password, id_user)
+// VALUES('test', 'test', 'test', 0);")
 
-// executeRequete("DELETE FROM blog.'user' where id_user=1");
+// executeRequete("INSERT INTO blog.'user')
+// (pseudo, email, password, id_user)
+// // VALUES('test1', 'test1', '123', 0);")
+
+// executeRequete("DELETE FROM blog.user where id_user=1");
+
+// executeRequete("INSERT INTO utilisateur
+// (slug, email, passwrd, is_admin)
+// VALUES('test', 'test', '12345', 1);")
+
+if ($_POST){
+    print_r($_POST);
+}
+
 
 ?>
 
 <?php include_once("./inc/templates/haut.php");?>
 <h1>Inscription</h1>
-<form class="row g-3 needs-validation" novalidate>
-  <div class="col-md-4">
-    <label for="validationCustom01" class="form-label">Prenom</label>
-    <input type="text" class="form-control" id="validationCustom01" value="Prenom" required>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
+<form method="post" >
+  <div class="form-group">
+    <label for="exampleInputEmail1">Votre pseudo</label>
+    <input name="pseudo" type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter pseudo">
   </div>
-  <div class="col-md-4">
-    <label for="validationCustom02" class="form-label">Nom</label>
-    <input type="text" class="form-control" id="validationCustom02" value="Nom de famille" required>
-    <div class="valid-feedback">
-      Looks good!
-    </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Votre mail</label>
+    <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
   </div>
-  <div class="col-md-4">
-    <label for="validationCustomUsername" class="form-label">Pseudo</label>
-    <div class="input-group has-validation">
-      <span class="input-group-text" id="inputGroupPrepend">@</span>
-      <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required>
-      <div class="invalid-feedback">
-        Choisissez un pseudo.
-      </div>
-    </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Votre mot de passe</label>
+    <input name="passwrd" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
   </div>
-  <div class="col-md-6">
-    <label for="validationCustom03" class="form-label">Ville</label>
-    <input type="text" class="form-control" id="validationCustom03" required>
-    <div class="invalid-feedback">
-      Choisissez une ville valide.
-    </div>
-  </div>
-  <div class="col-md-3">
-    <label for="validationCustom04" class="form-label">Etat</label>
-    <select class="form-select" id="validationCustom04" required>
-      <option selected disabled value="">Selectionnez...</option>
-      <option>...</option>
-    </select>
-    <div class="invalid-feedback">
-      Selectionnez un Etat valide.
-    </div>
-  </div>
-  <div class="col-md-3">
-    <label for="validationCustom05" class="form-label">Code Postale</label>
-    <input type="text" class="form-control" id="validationCustom05" required>
-    <div class="invalid-feedback">
-      Entrez un code postale valide.
-    </div>
-  </div>
-  
-  <div class="col-12">
-    <button class="btn btn-primary" type="submit">Submit form</button>
-  </div>
+  <button type="submit" class="btn btn-primary">S'inscrire</button>
 </form>
 <?php include_once("./inc/templates/bas.php");?>
